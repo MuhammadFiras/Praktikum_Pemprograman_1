9782e4263e960dd999a7af504866972f17805dc4
Muhammad Firas <muhammadfiras332@gmail.com>
2023-09-18 11:04:06 +0800

    Add files via upload

---
+++ Bahasa C/PRAK004-2210817110014-Muhammad_Firas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+
+    printf("######################\n");
+    printf("#                    #\n");
+    printf("#   Muhammad Firas   #\n");
+    printf("#   2210817110014    #\n");
+    printf("#                    #\n");
+    printf("######################\n");
+
+}
