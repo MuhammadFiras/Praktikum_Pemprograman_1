d486603448e992a4d34fc6c8bc27dcfd5ce2fda7
Muhammad Firas <muhammadfiras332@gmail.com>
2023-09-18 11:11:52 +0800

    Delete PRAK002-2210817110014-Muhammad_Firas.c

--- PRAK002-2210817110014-Muhammad_Firas.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main() {
-
-    printf("Selamat Pagi, Muhammad Firas\n");
-    printf("Selamat Siang, Muhammad Firas\n");
-    printf("Selamat Malam, Muhammad Firas\n");
-    
-}
